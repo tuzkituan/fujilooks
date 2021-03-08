f50892d10c47ab8966c994f65ffa96f96b8b8908
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-03-08 13:48:17 +0700

    integrate redux-saga into project

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import 'react-native-gesture-handler';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -8,51 +9,56 @@ import HomePage from './src/pages/Home';
 import SettingsPage from './src/pages/Settings';
 import PresetDetailPage from './src/pages/PresetDetail';
 
+import { Provider } from 'react-redux';
+import { store } from './src/store';
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        tabBarOptions={{
-          // showLabel: false,
-          showIcon: true,
-          activeBackgroundColor: '#000',
-          inactiveBackgroundColor: '#000',
-          activeTintColor: "teal"
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomePage}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            )
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
           }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsPage}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="account-settings" color={color} size={size} />
-            )
+          tabBarOptions={{
+            // showLabel: false,
+            showIcon: true,
+            activeBackgroundColor: '#000',
+            inactiveBackgroundColor: '#000',
+            activeTintColor: "teal"
           }}
-        />
-        <Tab.Screen
-          name="Preset Details"
-          component={PresetDetailPage}
-          options={{
-            tabBarButton: () => null,
-            tabBarVisible: false, // if you don't want to see the tab bar
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomePage}
+            options={{
+              tabBarLabel: 'Presets',
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="camera-iris" color={color} size={size} />
+              )
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsPage}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons name="account-settings" color={color} size={size} />
+              )
+            }}
+          />
+          <Tab.Screen
+            name="Preset Details"
+            component={PresetDetailPage}
+            options={{
+              tabBarButton: () => null,
+              tabBarVisible: false, // if you don't want to see the tab bar
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
